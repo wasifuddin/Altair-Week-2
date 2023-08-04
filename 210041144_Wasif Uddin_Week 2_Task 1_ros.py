6b5c2a750c1f0d5ee0989f66ab6cfe9a19ded8a3
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2023-08-05 05:16:06 +0600

    Create 210041144_Wasif Uddin_Week 2_Task 1_ros.py

---
+++ 210041144_Wasif Uddin_Week 2_Task 1_ros.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import rospy
+from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
+
+def pose_callback(pose:Pose):
+    cmd = Twist()
+    if pose.x > 9.0 or pose.x <2.0 or pose.y > 9.0 or pose.y < 2.0:
+        cmd.linear.x = 1.0
+        cmd.angular.z = 1.4
+    else:
+        cmd.linear.x = 5.0
+        cmd.angular.z = 0.0
+    pub.publish(cmd)
+
+if __name__ == '__main__':
+    # Initializing the node
+    rospy.init_node("turtle_controller")
+    # Creating the Publisher object
+    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size = 10)
+    # Creating the Subscriber object
+    sub = rospy.Subscriber("/turtle1/pose", Pose, callback = pose_callback)
+    rospy.loginfo("Node has been started")
+    # Using the spin function so that it runs continuously
+    rospy.spin()
