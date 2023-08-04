9f83ac851194b641e94f14a02c905a1069409a84
wasifuddin <79798771+wasifuddin@users.noreply.github.com>
2023-08-05 05:17:55 +0600

    Opencv Task helper function

---
+++ hsvval_extractor.py
@@ -0,0 +1,14 @@
+import cv2
+from cvzone.ColorModule import ColorFinder
+# Creating Color Finding Object
+myColorFinder = ColorFinder(True)
+
+while True:
+    # Reading the Image
+    img = cv2.imread('GOAT.jpg')
+    # Processing the img according to values in the trackbar
+    img, mask = myColorFinder.update(img)
+    # Showing the image
+    cv2.imshow("Color Detection", img)
+    # Setting cv2.waitkey to 1 to continuously see the updated image
+    cv2.waitKey(1)
